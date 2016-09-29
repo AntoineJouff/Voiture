2a5130f9ee6a62d3e325420731df956cd0fa78f1
Antoine <antoine.joufflineau@orange.fr>
2016-09-29 11:10:09 +0200

    Ajout des method

--- src/Store.java
+++ src/Store.java
@@ -1,13 +1,15 @@
-package com.iia.shop.entity;
+
+
+import com.iia.shop.entity.Car;
 
 public class Store {
 
 	public static void main (String args[]){
 	Car car0 = new Car(2, "red", "megan", 2500.99, "renault", 225);
 	Car car1 = new Car(5, "blue", "picasso", 8200.99, "citroen", 215);
-	Car car2 = new Car(9, "red", "megan", 3500.99, "renault", 200);
+	Car car2 = new Car(9, "red", "megane", 3500.99, "renault", 200);
 
-	Car tab [] = new Car[4];
+	Car tab [] = new Car[100];
 	tab[0]= car0;
 	tab[1]= car1;
 	tab[2]= car2;
--- src/com/iia/shop/entity/Car.java
+++ src/com/iia/shop/entity/Car.java
@@ -1,53 +1,63 @@
 package com.iia.shop.entity;
 
 public class Car {
-	
+
 	private int Year;
 	private String model;
 	private String color;
 	private Double price;
 	private int speed;
-	
-	
 
-	
 	private String marque;
+
 	public String getMarque() {
 		return marque;
 	}
+
 	public void setMarque(String marque) {
 		this.marque = marque;
 	}
+
 	public int getYear() {
 		return Year;
 	}
+
 	public void setYear(int year) {
 		Year = year;
 	}
+
 	public String getModel() {
 		return model;
 	}
+
 	public void setModel(String model) {
 		this.model = model;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
+
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
 	public int getSpeed() {
 		return speed;
 	}
+
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
+
 	public Car(int year, String model, String color, Double price, String marque, int speed) {
 		super();
 		Year = year;
@@ -56,22 +66,39 @@ public class Car {
 		this.price = price;
 		this.marque = marque;
 		this.speed = speed;
-	
+
 	}
-	
-	public static void Start (){
+
+	public static void Start() {
+		System.out.println("La voiture demarre");
+	}
+
+	public static void Stop() {
+		System.out.println("La voiture est arrete");
+	}
+
+	public static int SpeedUp(int speed) {
+		if (speed < 180) {
+			speed = speed + 20;
+			System.out.println("La vitesse de la voiture augmente. Elle roule a " + speed + "km");
+		} else {
+			System.out.println("La voiture est deja a sa vitesse maximal");
+		}
+		return speed;
+
+	}
+
+	public static int SpeedDown(int speed) {
+		if (speed > 20) {
+
+			speed = speed - 20;
+			System.out.println("La vitesse de la voiture diminue. Elle roule a " + speed + "Km");
+
+		} else {
+			System.out.println("La voiture est deja a l arret");
+		}
+		return speed;
 		
 	}
-	public static void Stop (){
-		
-	}
-	
-	public static void SpeedUp (){
-		
-	}
-	public static void SpeedDown (){
-	
-}
-	
-	
+
 }
